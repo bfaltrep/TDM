222562f10ec6bc706869789cd9fb0508f95c16f9
Bérénice Faltrept <berenice.faltrept@gmail.com>
2016-10-14 13:09:49 +0200

    TP6 combiner ok. Pas passage args

--- TP6/src/main/java/TP6.java
+++ TP6/src/main/java/TP6.java
@@ -24,7 +24,7 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class TP6 extends Configured implements Tool {
-	
+
 	public static class MyComparable implements Comparable{
 		private long _pop;
 		private String _town;
@@ -104,9 +104,44 @@ public class TP6 extends Configured implements Tool {
 			}
 		}
 	}
-	
+
 	public static class TP6Combiner extends Reducer<LongWritable, Text,LongWritable, Text>{
-		
+		public int size_top;
+		private TreeMap<MyComparable,String> top;
+		private Text value;
+		private LongWritable key;
+
+		public void setup(Context context){
+			size_top = context.getConfiguration().getInt("stop", 10);
+			top = new TreeMap<MyComparable,String>();
+			value = new Text();
+			key = new LongWritable();
+		}
+
+		public void reduce(LongWritable key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
+
+			for(Text val : value)
+			{
+				top.put(new MyComparable(key.get(), val.toString()), val.toString());
+
+				if (top.size() > size_top)
+					top.remove(top.firstKey());
+			}
+
+		}
+
+		@Override
+		public void cleanup(Context context) throws IOException, InterruptedException {
+
+			for (Entry<MyComparable,String> entry : top.entrySet())
+			{
+				key.set(entry.getKey().getPop());
+				value.set(entry.getValue());
+				context.write(key, value);
+			}
+		}
+
+
 	}
 
 	public static class TP6Reducer extends Reducer<LongWritable,Text,IntWritable,Text> {
@@ -163,16 +198,17 @@ public class TP6 extends Configured implements Tool {
 		}*/
 
 		Configuration conf = new Configuration();
-				
+
 		Job job = Job.getInstance(conf, "TP6");
 		job.setNumReduceTasks(1);
 		conf.setInt("stop", Integer.parseInt(args[2]));
 		job.setJarByClass(TP6.class);
-		
+
 
 		job.setMapperClass(TP6Mapper.class);
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(Text.class);
+		job.setCombinerClass(TP6Combiner.class);
 
 		job.setReducerClass(TP6Reducer.class);
 		job.setOutputKeyClass(IntWritable.class);
@@ -189,33 +225,33 @@ public class TP6 extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(new TP6(), args);
-		
+
 		try{
 			URI uri = new URI(args[1]+"/part-r-00000");
 			uri=uri.normalize();
-			FileSystem file = FileSystem.get(uri, new Configuration(), "kbaptist");
+			FileSystem file = FileSystem.get(uri, new Configuration(), "bfaltrep");
 			Path path = new Path(uri.getPath());
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(file.open(path)));
-			
+
 			String line;
 			line = buffer.readLine();
 			while (line!=null) {
 				System.out.println(line);
-                line = buffer.readLine();   
+				line = buffer.readLine();   
 			}
-			
+
 			URI uri_rep = new URI(args[1]);
 			uri_rep = uri_rep.normalize();
-			FileSystem _rep = FileSystem.get(uri_rep, new Configuration(), "kbaptist");
+			FileSystem _rep = FileSystem.get(uri_rep, new Configuration(), "bfaltrep");
 			Path path_rep = new Path(uri_rep.getPath());
-			
+
 			_rep.delete(path_rep, true);
-			
+
 		}
 		catch(Exception e){e.printStackTrace();}	
-				
+
 		System.exit(res);
-		
-		
+
+
 	}
 }
